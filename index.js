2ab2acd0d980aee45d12f85638155e2217b40418
Cripsyy <mariuscristiandragan22@gmail.com>
2023-11-05 16:00:09 +0200

    added json file and fetch method to read from it

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-let questions = {
-    questionText = [],
-    questionNumber, 
-};
+let jsonData;
+
+fetch('questions.json')
+  .then(response => response.json())
+  .then(data => {
+    jsonData = data;
+    test();
+  })
+  .catch(error => {
+    console.error('Error loading the file', error);
+  });
+
+function test(){
+  if(jsonData){
+    const question = jsonData[3];
+    const number = question.number;
+    console.log(number);
+  }else{
+    console.log("Data is not available yet")
+  }
+}
+
