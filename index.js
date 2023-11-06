b35e283fc844239926d3879ec0e7e49a15524315
Cripsyy <mariuscristiandragan22@gmail.com>
2023-11-06 10:09:52 +0200

    finished start quiz button

--- index.js
+++ index.js
@@ -20,3 +20,36 @@ function test(){
   }
 }
 
+let text = document.getElementsByClassName("main-text")[0];
+let quizButton = document.getElementsByClassName("main-button")[0];
+let nextButton = document.getElementsByClassName("next-button")[0];
+let previousButton = document.getElementsByClassName("previous-button")[0];
+let question = document.getElementsByClassName("question")[0];
+let answers = document.getElementsByClassName("answers")[0];
+
+function startQuiz(){
+    quizButton.classList.add("hidden");
+    text.classList.add("hidden");
+    question.classList.remove("hidden");
+    answers.classList.remove("hidden");
+    nextButton.classList.remove("hidden");
+    previousButton.classList.remove("hidden");
+    console.log("it works");
+    
+}
+
+function nextQuestion(){
+
+}
+
+function previousQuestion(){
+
+}
+
+if(quizButton){
+    quizButton.addEventListener("click", startQuiz);
+}else{
+    console.error("Element with class 'main-button' not found.");
+}
+
+
