86a070d0b980cc5977947271132b43e43296361f
Cripsyy <mariuscristiandragan22@gmail.com>
2023-11-09 16:45:21 +0200

    working previous and next question buttons

--- index.js
+++ index.js
@@ -1,64 +1,60 @@
 let jsonData;
+let index=0;
 
 fetch('questions.json')
   .then(response => response.json())
   .then(data => {
     jsonData = data;
     nextButton.addEventListener("click", nextQuestion);
+    previousButton.addEventListener("click", previousQuestion);
   })
   .catch(error => {
     console.error('Error loading the file', error);
   });
 
-function test(){
-  if(jsonData){
-    const question = jsonData[4];
-    const number = question.number;
-    console.log(number);
-    console.log(Object.keys(jsonData).lenght);
-    console.log(jsonData);
-    console.log(jsonData[7].question);
-  }else{
-    console.log("Data is not available yet")
-  }
-}
-
 let text = document.getElementsByClassName("main-text")[0];
 let quizButton = document.getElementsByClassName("main-button")[0];
 let nextButton = document.getElementsByClassName("next-button")[0];
 let previousButton = document.getElementsByClassName("previous-button")[0];
+let finishButton = document.getElementsByClassName("finish-button")[0];
 let question = document.getElementsByClassName("question")[0];
 let answers = document.getElementsByClassName("answers")[0];
 
 function startQuiz(){
-    quizButton.classList.add("hidden");
-    text.classList.add("hidden");
-    question.classList.remove("hidden");
-    answers.classList.remove("hidden");
-    nextButton.classList.remove("hidden");
-    previousButton.classList.remove("hidden");
-    console.log("it works");
+  quizButton.classList.add("hidden");
+  text.classList.add("hidden");
+  question.classList.remove("hidden");
+  answers.classList.remove("hidden");
+  nextButton.classList.remove("hidden");
+  console.log("it works");
 }
 
-let i=0;
-
 function nextQuestion(){
-    if(i === jsonData.lenght){
-        i=0;
-    }
-    question.innerText=jsonData[i].question;
-    i++;
-    console.log(i);
+  if(index === jsonData.length - 2){
+    finishButton.classList.remove("hidden");
+    nextButton.classList.add("hidden");
+  }
+  if(index !== 1){
+    previousButton.classList.remove("hidden");
+  }
+  index++;
+  console.log(index); 
+  question.innerText=jsonData[index].question;
 }
 
 function previousQuestion(){
-
+  if(index === 1){
+    previousButton.classList.add("hidden");
+  }
+  if(index < jsonData.length){
+    finishButton.classList.add("hidden");
+    nextButton.classList.remove("hidden");
+  }
+  index--;
+  console.log(index);
+  question.innerText=jsonData[index].question;
 }
 
-if(quizButton){
-    quizButton.addEventListener("click", startQuiz);
-}else{
-    console.error("Element with class 'main-button' not found.");
-}
+quizButton.addEventListener("click", startQuiz);
 
 
