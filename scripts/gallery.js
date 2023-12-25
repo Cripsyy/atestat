69a9455448d068a0374f2150fc7b4e91e9012d7f
Cripsyy <mariuscristiandragan22@gmail.com>
2023-12-25 14:29:25 +0200

    I don't know at this point

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,3 +1,12 @@
+console.log(cars);
+
+const buttons = document.querySelectorAll(".menu-button");
+
+
+function loadContent(){
+
+}
+
 gradientChange();
 
 function gradientChange(){
@@ -9,6 +18,8 @@ function gradientChange(){
     if(angle > 360){
       angle = 0;
     }
-    body.style.background = `rgba(0, 0, 0, 0) linear-gradient(${angle}deg, rgb(8, 7, 87) 0%, rgb(41, 38, 236) 100%) repeat scroll 0% 0% / auto padding-box border-box`;
+    body.style.background = `rgba(0, 0, 0, 0) linear-gradient(${angle}deg, rgb(16, 13, 219) 0%, rgb(43, 169, 243) 100%) repeat scroll 0% 0% / auto padding-box border-box`;
   }, 20);
 }
+
+
